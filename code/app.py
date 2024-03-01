27c70d6b0ed4b2ed656d47e5f3cf518e54246899
Adnan Azem <adnan.azem.250@gmail.com>
2024-03-01 15:18:51 +0200

    done

--- code/app.py
+++ code/app.py
@@ -5,24 +5,31 @@ from PIL import Image
 import joblib
 import numpy as np
 import pandas as pd
+import collections
+from sklearn.ensemble import RandomForestClassifier
 
 import math
 from collections import Counter
+import joblib
 import tldextract
+from publicsuffixlist import PublicSuffixList
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 
-dga_clf = joblib.load('../models/DGA.pkl')
-rf_clf = joblib.load('../models/DisicionTree.pkl')
+
+psl = PublicSuffixList()
+
+dga_clf = joblib.load('../models/DGA_Lightgbm.pkl')
+rf_clf = joblib.load('../models/RandomForest.pkl')
 alexa_vc = joblib.load('../models/alexa_vc.pkl')
 dict_cv = joblib.load('../models/dict_cv.pkl')
 def predict_url(url):
 
     url_features = extract_features(url)
-    # dga_featurs = extract_DGA_features(url)
+    dga_features = extract_DGA_features(url)
     rf_prediction = rf_clf.predict([url_features])
-    # dga_prediction = dga_clf.predict([dga_featurs])
-    prediction = rf_prediction[0]*0.6 + predict_domain_type(url)*0.4
+    dga_prediction = dga_clf.predict([dga_features])
+    prediction = rf_prediction*0.5 + dga_prediction*0.5
     return prediction
 
 def redirection(url):
@@ -210,82 +217,297 @@ def load_words():
 words = load_words()
 
 
-def predict_domain_type(domain):
-    # Extract features for the given domain
-    domain_name = get_domain_name(domain)
-    length = len(domain_name)
-    entropy_value = entropy(domain_name)
-
-    # Extract n-grams features using CountVectorizer
-    ngrams_features = alexa_vc.transform([domain_name])
-    ngrams_features = np.log10(np.asarray(ngrams_features.sum(axis=0)).flatten())
-
-    wordgram_feature = dict_cv.transform([domain_name])
-    wordgram_feature = dict_counts = np.log(np.asarray(wordgram_feature.sum(axis=0)).flatten())
-
-    # Create feature vector
-    feature_vector = np.concatenate(([length, entropy_value], ngrams_features, wordgram_feature))
-
-    # Reshape feature vector for prediction
-    # feature_vector = feature_vector.reshape(1, -1)
-
-    # Predict using the trained model
-    prediction = dga_clf.predict([feature_vector])
-
-    lb_dash = LabelEncoder()
-    # Convert prediction label back to original label
-    predicted_label = lb_dash.inverse_transform(prediction)[0]
-
-    return predicted_label
-
-# def clean_words_df(word):
-#     return str(word).strip().lower()
-#
-# def keep_alphanumeric(word):
-#     return str(word).isalpha()
-#
-# def extract_dictionary_ngrams():
-#     """Extract n-grams features from a dictionary of words."""
-#     words_df = pd.read_csv("../data/DGA/words.txt", names=["word"],
-#                              encoding="utf-8", header=None, dtype={"word": np.str_}, sep='\t')
-#     words_df = words_df[words_df["word"].map(lambda word: str(word).isalpha())]
-#     words_df = words_df.map(lambda word: str(word).strip().lower())
-#     words_df = words_df.dropna()
-#     words_df = words_df.drop_duplicates()
-#     dict_cv = CountVectorizer(analyzer="char", ngram_range=(3, 5), min_df=0.00001, max_df=1.0)
-#     words_counts_matrix = dict_cv.fit_transform(words_df["word"])
-#     dict_counts = np.log(np.asarray(words_counts_matrix.sum(axis=0)).flatten())
-#     words_ngrams_list = dict_cv.get_feature_names_out()
-#     return dict_counts, words_ngrams_list
-#
-# def extract_alexa_ngrams(data):
-#     """Extract n-grams features from Alexa data."""
-#     split_condition = data["label"] == "legit"
-#     legit = data[split_condition]
-#     alexa_vc = CountVectorizer(analyzer="char", ngram_range=(3, 5), min_df=0.00001, max_df=1.0)
-#     counts_matrix = alexa_vc.fit_transform(legit["domain"])
-#     alexa_counts = np.log10(np.asarray(counts_matrix.sum(axis=0)).flatten())
-#     ngrams_list = alexa_vc.get_feature_names_out()
-#     return alexa_counts, ngrams_list
+def load_topLevelDomain():
+    """ Load Valid Top Level Domains data """
+    topLevelDomain = []
+    with open('../data/DGA/tlds-alpha-by-domain.txt', 'r') as content:
+        for line in content:
+            topLevelDomain.append((line.strip('\n')))
+    return topLevelDomain
 
 
-def extract_DGA_features(domain):
+topLevelDomain = load_topLevelDomain()
+
+
+def ignoreVPS(domain):
+    # Return the rest of domain after ignoring the Valid Public Suffixes:
+    validPublicSuffix = '.' + psl.publicsuffix(domain)
+    if len(validPublicSuffix) < len(domain):
+        # If it has VPS
+        subString = domain[0: domain.index(validPublicSuffix)]
+    elif len(validPublicSuffix) == len(domain):
+        return 0
+    else:
+        # If not
+        subString = domain
+
+    return subString
+
+
+def domain_length(domain):
+    # Generate Domain Name Length (DNL)
+    return len(domain)
+
+
+def subdomains_number(domain):
+    # Generate Number of Subdomains (NoS)
+    subdomain = ignoreVPS(domain)
+    return subdomain.count('.') + 1
+
+
+def subdomain_length_mean(domain):
+    # Generate Subdomain Length Mean (SLM)
+    subdomain = ignoreVPS(domain)
+    result = (len(subdomain) - subdomain.count('.')) / (subdomain.count('.') + 1)
+    return result
+
+
+def has_www_prefix(domain):
+    # Generate Has www Prefix (HwP)
+    if domain.split('.')[0] == 'www':
+        return 1
+    else:
+        return 0
+
+
+def has_hvltd(domain):
+    # Generate Has a Valid Top Level Domain (HVTLD)
+    if domain.split('.')[len(domain.split('.')) - 1].upper() in topLevelDomain:
+        return 1
+    else:
+        return 0
+
+
+def contains_single_character_subdomain(domain):
+    # Generate Contains Single-Character Subdomain (CSCS)
+    domain = ignoreVPS(domain)
+    str_split = domain.split('.')
+    minLength = len(str_split[0])
+    for i in range(0, len(str_split) - 1):
+        minLength = len(str_split[i]) if len(str_split[i]) < minLength else minLength
+    if minLength == 1:
+        return 1
+    else:
+        return 0
+
+
+def contains_TLD_subdomain(domain):
+    # Generate Contains TLD as Subdomain (CTS)
+    subdomain = ignoreVPS(domain)
+    str_split = subdomain.split('.')
+    for i in range(0, len(str_split) - 1):
+        if str_split[i].upper() in topLevelDomain:
+            return 1
+    return 0
+
+
+def underscore_ratio(domain):
+    # Generate Underscore Ratio (UR) on dataset
+    subString = ignoreVPS(domain)
+    result = subString.count('_') / (len(subString) - subString.count('.'))
+    return result
+
+
+def contains_IP_address(domain):
+    # Generate Contains IP Address (CIPA) on datasetx
+    splitSet = domain.split('.')
+    for element in splitSet:
+        if (re.match("\d+", element)) is None:
+            return 0
+    return 1
+
+
+def contains_digit(domain):
+    """
+     Contains Digits
+    """
+    subdomain = ignoreVPS(domain)
+    for item in subdomain:
+        if item.isdigit():
+            return 1
+    return 0
+
+
+def vowel_ratio(domain):
+    """
+    calculate Vowel Ratio
+    """
+    VOWELS = set('aeiou')
+    v_counter = 0
+    a_counter = 0
+    subdomain = ignoreVPS(domain)
+    for item in subdomain:
+        if item.isalpha():
+            a_counter += 1
+            if item in VOWELS:
+                v_counter += 1
+    if a_counter > 1:
+        ratio = v_counter / a_counter
+        return ratio
+
+
+def digit_ratio(domain):
+    """
+    calculate digit ratio
+    """
+    d_counter = 0
+    counter = 0
+    subdomain = ignoreVPS(domain)
+    for item in subdomain:
+        if item.isalpha() or item.isdigit():
+            counter += 1
+            if item.isdigit():
+                d_counter += 1
+    if counter > 1:
+        ratio = d_counter / counter
+        return ratio
+
+
+def prc_rrc(domain):
+    """
+    calculate the Ratio of Repeated Characters in a subdomain
+    """
+    subdomain = ignoreVPS(domain)
+    #   subdomain =''.join(re.findall('[a-zA-Z]+', subdomain))
+    subdomain = re.sub("[.]", "", subdomain)
+    char_num = 0
+    repeated_char_num = 0
+    d = collections.defaultdict(int)
+    for c in list(subdomain):
+        d[c] += 1
+    for item in d:
+        char_num += 1
+        if d[item] > 1:
+            repeated_char_num += 1
+    ratio = repeated_char_num / char_num
+    return ratio
+
+
+def prc_rcc(domain):
+    """
+    calculate the Ratio of Consecutive Consonants
+    """
+    VOWELS = set('aeiou')
+    counter = 0
+    cons_counter = 0
+    subdomain = ignoreVPS(domain)
+    #   subdomain =''.join(re.findall('[a-zA-Z]+', subdomain))
+    i = 0
+    for item in subdomain:
+        if item.isalpha() and item not in VOWELS:
+            counter += 1
+        else:
+            if counter > 1:
+                cons_counter += counter
+            counter = 0
+        i += 1
+    if i == len(subdomain) and counter > 1:
+        cons_counter += counter
+    ratio = cons_counter / len(subdomain)
+    return ratio
+
+
+def prc_rcd(domain):
+    """
+    calculate the ratio of consecutive digits
+    """
+    counter = 0
+    digit_counter = 0
+    subdomain = ignoreVPS(domain)
+    #   subdomain =''.join(re.findall('[a-zA-Z]+', subdomain))
+    i = 0
+    for item in subdomain:
+        if item.isdigit():
+            counter += 1
+        else:
+            if counter > 1:
+                digit_counter += counter
+            counter = 0
+        i += 1
+    if i == len(subdomain) and counter > 1:
+        digit_counter += counter
+    ratio = digit_counter / len(subdomain)
+    return ratio
+
+
+def prc_entropy(domain):
+    """
+    calculate the entropy of subdomain
+    :param domain_str: subdomain
+    :return: the value of entropy
+    """
+    subdomain = ignoreVPS(domain)
+    # get probability of chars in string
+    prob = [float(subdomain.count(c)) / len(subdomain) for c in dict.fromkeys(list(subdomain))]
+
+    # calculate the entropy
+    entropy = - sum([p * math.log(p) / math.log(2.0) for p in prob])
+    return entropy
+
+
+def extract_DGA_features(url):
     dga_features = []
 
-    i = get_domain_name(domain)
+    i = domain_length(url)
     dga_features.append(i)
 
-    i = len(domain)
+    i = subdomains_number(url)
     dga_features.append(i)
 
-    i = entropy(domain)
+    i = subdomain_length_mean(url)
     dga_features.append(i)
 
-    # i = extract_dictionary_ngrams(domain)[0]
-    # dga_features.append(i)
-    #
-    # i = extract_alexa_ngrams(domain)[0]
-    # dga_features.append(i)
+    i = has_www_prefix(url)
+    dga_features.append(i)
+
+    i = has_hvltd(url)
+    dga_features.append(i)
+
+    i = contains_single_character_subdomain(url)
+    dga_features.append(i)
+
+    i = contains_TLD_subdomain(url)
+    dga_features.append(i)
+
+    i = underscore_ratio(url)
+    dga_features.append(i)
+
+    i = contains_IP_address(url)
+    dga_features.append(i)
+
+    i = contains_digit(url)
+    dga_features.append(i)
+
+    i = vowel_ratio(url)
+    dga_features.append(i)
+
+    i = digit_ratio(url)
+    dga_features.append(i)
+
+    i = prc_rrc(url)
+    dga_features.append(i)
+
+    i = prc_rcc(url)
+    dga_features.append(i)
+
+    i = prc_rcd(url)
+    dga_features.append(i)
+
+    i = prc_entropy(url)
+    dga_features.append(i)
+
+    domain_name = get_domain_name(url)
+    # dga_features.append(domain_name)
+
+    ngrams_features = alexa_vc.transform([url])
+    alexa_counts = np.log10(np.asarray(ngrams_features.sum(axis=0)).flatten())
+    alexa = alexa_counts * alexa_vc.transform([domain_name]).T
+    dga_features.append(alexa[0])
+
+    wordgram_feature = dict_cv.transform([url])
+    dict_counts = np.log(np.asarray(wordgram_feature.sum(axis=0)).flatten())
+    wordgram = dict_counts * dict_cv.transform([domain_name]).T
+    dga_features.append(wordgram[0])
 
     return dga_features
 
@@ -299,11 +521,7 @@ def main():
     with mid:
         st.image(image2, width=150)
 
-    st.title("Malicious URL Detection")
-  #   """Phishing URL Detection App
-  #   With Streamlit
-  #
-  # """
+    st.header(":skull: Malicious URL Detection :skull:",divider='rainbow')
 
     html_temp = """
   <div style="background-color:blue;padding:10px">
@@ -311,14 +529,13 @@ def main():
   </div>
 
   """
-    # st.markdown(html_temp, unsafe_allow_html=True)
     url = st.text_input("Enter website address or URL")
 
     if st.button("Predict"):
         result = predict_url(url)
         print(result)
 
-        if result >= 0.6:
+        if result > 0.5:
             prediction = 'phishing website'
             img = 'bad.png'
             st.warning('The URL: {}  is a malicious URL'.format(prediction))
@@ -330,9 +547,40 @@ def main():
         load_images(img)
 
     st.subheader("About")
-    st.info("Adnan Azem & Jode Shibli Final Project")
-    st.info("Link to the project:  " + "")
 
+    # Create a container
+    with st.container():
+        # Customize the background color, padding, and some styling
+        st.markdown("""
+            <style>
+            .beautiful-container {
+                background-color: #f0f2f6;
+                border-radius: 10px;
+                padding-left: 20px;
+                padding-bottom: 10px
+                padding-top: 10px
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            }
+            .beautiful-title {
+                color: #333;
+                font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            }
+            </style>
+            <div class="beautiful-container">
+                <b class="beautiful-title">Created by:</b>
+                <ul>
+                <li> Adnan Azem </li>
+                <li> Jode Shibli </li>
+                </ul>
+
+            </div>
+            """, unsafe_allow_html=True)
+
+
+    st.link_button("Go to Project", "https://github.com/AdnanAzem/Malicious-URL-Detection.git")
+    link = '''
+    <a href = "https://github.com/AdnanAzem/Malicious-URL-Detection.git">click here</a>
+    '''
 
 if __name__ == '__main__':
     main()
